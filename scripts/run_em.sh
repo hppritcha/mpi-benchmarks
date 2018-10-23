8926c7a5209938e260a97e6d4abfe35a137038bb
Howard Pritchard <howardp@lanl.gov>
2018-10-23 15:59:09 -0600

    fix up the run_em.sh script some
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- scripts/run_em.sh
+++ scripts/run_em.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -f
 
 NAMES="Allreduce Allgather Alltoall"
+NRANKS=4
 for name in $NAMES
 do
     echo $name.lengths
-    mpirun -np 4 ../src_c/IMB-MPI1 -msglen $name.lengths $name
+    mpirun -np $NRANKS ../src_c/IMB-MPI1 -npmin $NRANKS -msglen $name.lengths $name
 done
 #
 # barrier is a special case with no lengths
-mpirun -np 4 ../src_c/IMB-MPI1 Barrier
+mpirun -np $NRANKS ../src_c/IMB-MPI1 -npmin $NRANKS Barrier
